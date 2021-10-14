74e83cfee53bd4413e8392a66f6df67294f9a416
rgiovani <ronaldo-giovani07@hotmail.com>
2021-10-13 21:30:53 -0300

    instalando cors

--- estudo/semana 2/Tipando o Javascript/api-pets/index.ts
+++ estudo/semana 2/Tipando o Javascript/api-pets/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import notesRoutes from './routes/notesRoutes'
+import cors from 'cors'
 
 const app = express()
 const PORT = 3000
 
 app.use(express.static('www'))
 app.use(express.json())
+app.use(cors())
 
 notesRoutes(app)
 
