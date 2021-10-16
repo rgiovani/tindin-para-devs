7593eb69ec5086df2f305bf0164f0c48d1d0291e
rgiovani <ronaldo-giovani07@hotmail.com>
2021-10-16 12:11:29 -0300

    Estruturando a validacao dos erros de notas

--- estudo/semana 2/api-anotacoes-tipada/controllers/note.ts
+++ estudo/semana 2/api-anotacoes-tipada/controllers/note.ts
@@ -1,69 +1,56 @@
 import { Request, Response } from 'express';
+
 import * as noteService from '../services/note';
+import { statusError } from '../libs/bindError'
 
 const listFavorites = (req: Request<any>, res: Response<any>) => {
     const notes = noteService.findFavorites()
-
     return res.json(notes)
 }
 
 const list = (req: Request<any>, res: Response<any>) => {
     const notes = noteService.list()
-
     return res.json(notes)
 }
 
 const get = (req: Request<any>, res: Response<any>) => {
-    try {
-        const id = req.params.id
-        if (!id) return res.status(400).json({ message: 'Informe o campo id!' })
+    const id = req.params.id
+    if (!id) return res.status(400).json({ message: 'Informe o campo id!' })
 
-        const note = noteService.get(id)
-        res.json(note)
-    } catch (error) {
-        console.log(error)
-        return res.status(400).json({ message: error })
-    }
+    validateResponse(res, noteService.get, id);
 }
 
 const create = (req: Request<any>, res: Response<any>) => {
-    try {
-        const { title, description } = req.body
+    const { title, description } = req.body
 
-        const note = noteService.create({ title, description })
-
-        res.json(note)
-    } catch (error) {
-        return res.status(400).json({ message: error })
-    }
+    validateResponse(res, noteService.create, { title, description });
 }
 
 const update = (req: Request<any>, res: Response<any>) => {
-    try {
-        const { id, title, description, isFav } = req.body
-        if (!id) {
-            return res.status(400).json({ message: 'Informe o campo id!' })
-        }
+    const { id, title, description, isFav } = req.body
 
-        const noteUpdated = noteService.update({ id, title, description, isFav })
-        res.json(noteUpdated)
-    } catch (error) {
-        return res.status(400).json({ message: error })
+    if (!id) {
+        return res.status(400).json({ message: 'Informe o campo id!' })
     }
+
+    validateResponse(res, noteService.update, { id, title, description, isFav })
 }
 
 const remove = (req: Request<any>, res: Response<any>) => {
+    const { id } = req.body
+
+    if (!id) {
+        return res.status(400).json({ message: 'Informe o campo id!' })
+    }
+
+    validateResponse(res, noteService.remove, id)
+}
+
+function validateResponse(res: Response<any>, fn: any, param: any) {
     try {
-        const { id } = req.body
-
-        if (!id) {
-            return res.status(400).json({ message: 'Informe o campo id!' })
-        }
-        noteService.remove(id)
-
-        res.json({ success: true })
-    } catch (error) {
-        return res.status(400).json({ message: error })
+        res.json(fn(param))
+    } catch (error: any) {
+        return statusError(res, error)
     }
 }
 
