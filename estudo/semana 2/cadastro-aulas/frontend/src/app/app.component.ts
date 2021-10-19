675cbd310fac318f91072536ff35a2018788193b
rgiovani <ronaldo-giovani07@hotmail.com>
2021-10-19 02:26:13 -0300

    Estilizando tela de login

--- estudo/semana 2/cadastro-aulas/frontend/src/app/app.component.ts
+++ estudo/semana 2/cadastro-aulas/frontend/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  constructor(private elementRef: ElementRef) {
+
+  }
+  ngAfterViewInit(): void {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#A89FE9';
+  }
+
   title = 'Cadastro de aulas';
 }
